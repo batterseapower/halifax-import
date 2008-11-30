1b773b72c88821c3fd736711c7de050bfdb632eb
Max Bolingbroke <batterseapower@hotmail.com>
2008-11-30 23:35:41 +0000

    Make statement parsing test pass

--- Ledger/Halifax/Tests.hs
+++ Ledger/Halifax/Tests.hs
@@ -1,17 +1,20 @@
 module Main where
 
-import Ledger.Halifax.Core
-import Ledger.Halifax.Rules
-import Ledger.Halifax.RulesParser
-import Ledger.Halifax.StatementParser
-
 import Paths_halifax_ledger
 
 import Control.Monad
 
+import Data.Time.Calendar
+import Data.Time.Clock
+
 import Test.Framework
 import Test.Framework.Providers.HUnit
 import Test.HUnit hiding (Test)
+
+import Ledger.Halifax.Core
+import Ledger.Halifax.Rules
+import Ledger.Halifax.RulesParser
+import Ledger.Halifax.StatementParser
 
 
 main :: IO ()
@@ -29,8 +32,95 @@ test_statement_parse :: Assertion
 test_statement_parse = do
     page_1 <- readTestData "Statement-Page-1.html"
     page_2 <- readTestData "Statement-Page-2.html"
-    let parsed = parseStatement [page_1, page_2]
-    parsed @?= undefined
+    let (account, transactions) = parseStatement [page_1, page_2]
+        utcDay day = UTCTime { utctDay = day, utctDayTime = secondsToDiffTime 0 }
+    account @?= Account {
+            acc_sort_code = "11-22-33",
+            acc_number = 1234567,
+            acc_roll_number = "D/12345678-9"
+        }
+    transactions @?= [
+            Transaction {
+                tr_time = utcDay $ fromGregorian 2008 11 29,
+                tr_comments = [],
+                tr_counterparty = "ATM",
+                tr_category = Nothing,
+                tr_type = Debit,
+                tr_amount = 1
+            },
+            Transaction {
+                tr_time = utcDay $ fromGregorian 2008 11 24,
+                tr_comments = ["Location 1"],
+                tr_counterparty = "Debit Card",
+                tr_category = Nothing,
+                tr_type = Debit,
+                tr_amount = 101 / 100
+            },
+            Transaction {
+                tr_time = utcDay $ fromGregorian 2008 11 24,
+                tr_comments = ["Location 2"],
+                tr_counterparty = "Debit Card",
+                tr_category = Nothing,
+                tr_type = Debit,
+                tr_amount = 102 / 100
+            },
+            Transaction {
+                tr_time = utcDay $ fromGregorian 2008 11 22,
+                tr_comments = [],
+                tr_counterparty = "Deposit Cheque",
+                tr_category = Nothing,
+                tr_type = Credit,
+                tr_amount = 104 / 100
+            },
+            Transaction {
+                tr_time = utcDay $ fromGregorian 2008 11 17,
+                tr_comments = [],
+                tr_counterparty = "Direct Debit",
+                tr_category = Nothing,
+                tr_type = Debit,
+                tr_amount = 105 / 100
+            },
+            Transaction {
+                tr_time = utcDay $ fromGregorian 2008 11 17,
+                tr_comments = [],
+                tr_counterparty = "ATM",
+                tr_category = Nothing,
+                tr_type = Debit,
+                tr_amount = 106 / 100
+            },
+            Transaction {
+                tr_time = utcDay $ fromGregorian 2008 11 17,
+                tr_comments = ["Location 1"],
+                tr_counterparty = "Debit Card",
+                tr_category = Nothing,
+                tr_type = Debit,
+                tr_amount = 107 / 100
+            },
+            Transaction {
+                tr_time = utcDay $ fromGregorian 2008 10 07,
+                tr_comments = ["Location 2"],
+                tr_counterparty = "Debit Card",
+                tr_category = Nothing,
+                tr_type = Debit,
+                tr_amount = 109 / 100
+            },
+            Transaction {
+                tr_time = utcDay $ fromGregorian 2008 10 07,
+                tr_comments = [],
+                tr_counterparty = "Cash",
+                tr_category = Nothing,
+                tr_type = Credit, 
+                tr_amount = 110 / 100
+            },
+            Transaction {
+                tr_time = utcDay $ fromGregorian 2008 10 06,
+                tr_comments = ["Location 1"],
+                tr_counterparty = "Debit Card",
+                tr_category = Nothing,
+                tr_type = Debit,
+                tr_amount = 111 / 100
+            }
+        ]
 
 test_rules_parse :: Assertion
 test_rules_parse = do
